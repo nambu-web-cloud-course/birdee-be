792e21c7c7b4b1f9d8eeb7d5b351a1288901f7f8
j1sooko <doubletry23@gmail.com>
2023-11-09 17:21:22 +0900

    feat: 회원가입, 로그인

--- routers/auth_router.js
+++ routers/auth_router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET || "secret";
+
 // model
 const { User } = require('../models');
 
@@ -21,6 +24,33 @@ router.post('/member', async (req, res) => {
         res.status(500).send({ success: false, message: error.message });
     }
 
+});
+
+router.post('/login', async (req, res) => {
+    const user = req.body;
+    
+    options = { 
+        attributes: ['password'],
+        where: {
+            user_id: user.user_id
+        }
+    }; 
+
+    try {
+        const result = await User.findOne(options);
+        if (result) {
+            const compared = await bcrypt.compare(user.password, result.password);
+            console.log(`${user.password} : ${result.password}, ${compared} `)
+            if (compared) {
+                const token = jwt.sign({ uid: user.user_id, rol: 'user'}, secret);
+                res.send({ success: true, user_id: user.user_id, toekn: token });
+            } else {
+                res.send({ "success": false, message: "해당 사용자가 존재하지 않거나 비밀번호가 틀렸습니다." });
+            }
+        }
+    } catch(error) {
+        res.send({ success: false, message: error.message });
+    }
 });
 
 module.exports = router;
