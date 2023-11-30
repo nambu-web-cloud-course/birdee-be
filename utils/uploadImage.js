3de68482c26ee2f3360134a076346b7c8c3aed4c
j1sooko <doubletry23@gmail.com>
2023-11-30 16:53:35 +0900

    Fix: azureStorage 연결 정보 수정

--- utils/uploadImage.js
+++ utils/uploadImage.js
@@ -6,15 +6,11 @@ const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStora
 const getBlobName = (req, file) => {
     const ext = path.extname(file.originalname);
     console.log(path.basename(file.originalname, ext));
-    const blobName = path.basename(file.originalname, ext) + Date.now() + ext; // 파일명과 확장자 분리 후 현재시각을 붙임
-    console.log(blobName);
     return path.basename(file.originalname, ext) + Date.now() + ext; // 파일명과 확장자 분리 후 현재시각을 붙임
 };
 
 const azureStorage = new MulterAzureStorage({
     connectionString: process.env.CONNECTION_STRING,
-    accessKey: process.env.AZURE_KEY,
-    accountName: process.env.ACCOUNT_NAME,
     containerName: 'images', 
     blobName: getBlobName,
     limits: { fileSize: 10 * 1024 * 1024 }
